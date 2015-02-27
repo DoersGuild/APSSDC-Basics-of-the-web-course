a2f5c29000faedfa7fd7d5a6949e60530e39c632
Sathvik P <psathvik@gmail.com>
2015-02-27 09:59:33 +0530

    JavaScript base

---
+++ JavaScript1/js/main.js
@@ -0,0 +1,30 @@
+console.log('Hello everyone!'); // String
+console.log(50); // Number - Integer
+console.log(50.56); // Number - Float
+console.log([5, 1, 3, 68, 34]); // Array
+console.log({
+    'a': 1,
+    'b': 2
+}); // Object
+// int i = 0;
+var i = 0;
+console.log('i = ', i);
+i = 'i changed into a string';
+console.log('i = ', i);
+var arr = [5, 2573, 3, 68, 34];
+console.log('arr[1] = ', arr[1]);
+arr[1] = 75;
+console.log('arr[1] = ', arr[1]);
+console.log('arr = ', arr);
+arr.push(125);
+console.log('arr = ', arr);
+var obj = {
+    'a': 1,
+    'b': 2,
+    'key': 'value'
+};
+console.log('obj = ', obj);
+console.log('obj["a"] = ', obj['a']);
+console.log('obj["b"] = ', obj['b']);
+console.log('obj["key"] = ', obj['key']);
+console.log('obj.key = ', obj.key);
