e90d9d848c471805ab21bd48dbd09c938d775534
Sathvik P <psathvik@gmail.com>
2015-02-27 11:06:33 +0530

    JavaScript base+

--- JavaScript1/js/main.js
+++ JavaScript1/js/main.js
@@ -18,13 +18,33 @@ console.log('arr[1] = ', arr[1]);
 console.log('arr = ', arr);
 arr.push(125);
 console.log('arr = ', arr);
+// Object
 var obj = {
     'a': 1,
     'b': 2,
-    'key': 'value'
+    'key': 'value 1'
 };
 console.log('obj = ', obj);
 console.log('obj["a"] = ', obj['a']);
 console.log('obj["b"] = ', obj['b']);
 console.log('obj["key"] = ', obj['key']);
 console.log('obj.key = ', obj.key);
+obj.key = 'value 2';
+console.log('obj.key = ', obj.key);
+console.log('obj = ', obj);
+obj.key2 = 'value for key2';
+console.log('obj = ', obj);
+var str = 'hello';
+console.log('str = ', str);
+console.log('str.length = ', str.length);
+console.log('str.toUpperCase() = ', str.toUpperCase());
+console.log('document.body = ', document.body);
+console.log('document.getElementById("main") = ', document.getElementById("main"));
+console.log('document.getElementById("myParagraph") = ', document.getElementById("myParagraph"));
+var myParagraph = document.getElementById("myParagraph");
+console.log('myParagraph = ', myParagraph);
+console.log('myParagraph.innerHTML = ', myParagraph.innerHTML);
+myParagraph.innerHTML = 'Hello myParagraph';
+console.log('myParagraph.innerHTML = ', myParagraph.innerHTML);
+myParagraph.innerHTML = 'Hello <b>myParagraph</b>';
+console.log('myParagraph.innerHTML = ', myParagraph.innerHTML);
